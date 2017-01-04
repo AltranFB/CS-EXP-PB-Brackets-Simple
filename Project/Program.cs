97493f627296f3db696ee7f9452505728ae497f7
Grokwik <frederic@bodin.site>
2017-01-04 13:01:04 +0100

    Unit tests added

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,19 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Project
 {
     public class Solution
     {
-        public static void DoIt()
+        public static bool DoIt(string expression)
         {
+            //  the expression is given as argument, you must return the boolean answer:
+            //  true if the brackets/parenthesis & curly brackets are correctly paired
+
+            //  Type your code here
+
+            return true;
         }
 
+        //  Auto generated code, please don't modify the code below
         static void Main(string[] args)
         {
+            string expression = Console.ReadLine();
+            Console.WriteLine(DoIt(expression));
         }
     }
 }
--- UnitTests/Tests.cs
+++ UnitTests/Tests.cs
@@ -9,7 +9,53 @@ namespace Project
         [TestMethod]
         public void Test1()
         {
-            Solution.DoIt();
+            Check.That(Solution.DoIt("[()]")).IsTrue();
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            Check.That(Solution.DoIt("(()[])")).IsTrue();
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            Check.That(Solution.DoIt("([)]")).IsFalse();
+        }
+        [TestMethod]
+        public void Test4()
+        {
+            Check.That(Solution.DoIt("((")).IsFalse();
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            Check.That(Solution.DoIt("[(()])")).IsFalse();
+        }
+        [TestMethod]
+        public void Test6()
+        {
+            Check.That(Solution.DoIt("([(([[(([]))]]))])")).IsTrue();
+        }
+        [TestMethod]
+        public void Test7()
+        {
+            Check.That(Solution.DoIt("[](()()[[]])()[]([])")).IsTrue();
+        }
+        [TestMethod]
+        public void Test8()
+        {
+            Check.That(Solution.DoIt("([((([(([]))])))))])")).IsFalse();
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            Check.That(Solution.DoIt("[](()()[[]])[][[([])")).IsFalse();
         }
     }
 }
+/*
+
+ */
