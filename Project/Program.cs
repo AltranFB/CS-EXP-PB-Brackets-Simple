9a8183e5ebf90d25de264a207e3fc2a628af4489
Grokwik <frederic@bodin.site>
2017-01-10 13:03:00 +0100

    clean up

--- Project/Program.cs
+++ Project/Program.cs
@@ -11,39 +11,8 @@ namespace Project
     {
         static List<char> Possible;
 
-        public static bool Check(string str)
-        {
-            return DoIt(str);
-        }
         public static bool DoIt(string str)
         {
-            if (str == null)
-                return true;
-
-            var idx = 0;
-            Possible = new List<char>();
-
-            while (idx < str.Length)
-            {
-                switch (str[idx])
-                {
-                    case '(':
-                        Possible.Add(')');
-                        break;
-                    case '[':
-                        Possible.Add(']');
-                        break;
-                    case ']':
-                    case ')':
-                        if ((Possible.Count == 0)
-                          ||(Possible.ElementAt(Possible.Count - 1) != str[idx]))
-                            return false;
-                        Possible.RemoveAt(Possible.Count - 1);
-                        break;
-                }
-                idx++;
-            }
-            return (Possible.Count == 0);
         }
 
         //  Auto generated code, please don't modify the code below
